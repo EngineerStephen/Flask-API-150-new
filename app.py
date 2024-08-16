29c1a4b8cd2925177da398c3e10c6892ba8ea4e1
allan <allansaleh95@gmail.com>
2024-08-16 17:12:04 -0400

    api documentation

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from database import db
 from models.schemas import ma
-from limiter import limiter
+# from limiter import limiter
 from caching import cache
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from models.customer import Customer
 from models.product import Product
@@ -12,6 +13,12 @@ from routes.customerBP import customer_blueprint
 from routes.productBP import product_blueprint
 from routes.orderBP import order_blueprint
 
+#SWAGGER
+SWAGGER_URL = '/api/docs' # URL endpoint for Swagger API documentation
+API_URL = '/static/swagger.yaml'
+
+swagger_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name':"E-commerce API"})
+
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -19,7 +26,7 @@ def create_app(config_name):
     app.config.from_object(f'config.{config_name}')
     db.init_app(app)
     ma.init_app(app)
-    limiter.init_app(app)
+    # limiter.init_app(app)
     cache.init_app(app)
 
     print('Running')
@@ -29,9 +36,10 @@ def blueprint_config(app):
     app.register_blueprint(customer_blueprint, url_prefix='/customers')
     app.register_blueprint(product_blueprint, url_prefix='/products')
     app.register_blueprint(order_blueprint, url_prefix='/orders')
+    app.register_blueprint(swagger_blueprint, url_prefix=SWAGGER_URL)
 
-def rate_limit_config():
-    limiter.limit("3 per day")(customer_blueprint)
+# def rate_limit_config():
+#     limiter.limit("200 per day")
 
 if __name__ == '__main__':
     app = create_app('DevelopmentConfig')
